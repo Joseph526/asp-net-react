915f9810ea58519165b1f911871ccbc7fc9f6904
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2019-05-06 22:43:24 -0400

    Configure React.AspNet dependency

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,14 @@
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
 
 namespace asp_net_react
 {
@@ -20,6 +24,13 @@ namespace asp_net_react
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+                .AddChakraCore();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
@@ -44,6 +55,26 @@ namespace asp_net_react
             }
 
             app.UseHttpsRedirection();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+            // If you want to use server-side rendering of React components,
+            // add all the necessary JavaScript files here. This includes
+            // your components as well as all of their dependencies.
+            // See http://reactjs.net/ for more information. Example:
+            //     config
+            //         .AddScript("~/Scripts/First.jsx")
+            //         .AddScript("~/Scripts/Second.jsx");
+
+            // If you use an external build too (for example, Babel, Webpack,
+            // Browserify or Gulp), you can improve performance by disabling
+            // ReactJS.NET's version of Babel and loading the pre-transpiled
+            // scripts. Example:
+            //     config
+            //         .SetLoadBabel(false)
+            //         .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
